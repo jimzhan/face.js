53140f2e20891afd1fe6e973fd2f7709989599ce
Jim Zhan <jim.zhan@me.com>
2017-09-26 21:00:42 +0800

    [WIP] added ant.d supports

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
+import 'antd/lib/button/style'
 import React from 'react'
 import { render } from 'react-dom'
 import DevTools from 'mobx-react-devtools'
+import Nav from './modules/Nav'
 
 render(
   <div>
     <h2>Hello World</h2>
+    <Nav />
     <DevTools />
   </div>,
   document.getElementById('root')
